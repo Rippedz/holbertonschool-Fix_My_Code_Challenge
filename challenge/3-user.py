e22f14ae1ebd25c56ed2689c19d7c18792538421
Rippedz <ttfoot12@gmail.com>
2024-09-12 22:48:05 +0200

    chall

--- challenge/3-user.py
+++ challenge/3-user.py
@@ -18,7 +18,7 @@ class User():
     def __init__(self):
         """
         Initialize a new user:
-        - assigned a unique `id`
+        - assigned an unique `id`
         """
         self.id = str(uuid.uuid4())
 
@@ -35,7 +35,7 @@ class User():
         Password setter:
         - `None` if `pwd` is `None`
         - `None` if `pwd` is not a string
-        - Hash `pwd` in MD5 before assigning to `__password`
+        - Hash `pwd` in MD5 before assign to `__password`
         """
         if pwd is None or type(pwd) is not str:
             self.__password = None
@@ -78,23 +78,26 @@ if __name__ == '__main__':
 
     user_2.password = None
     if user_2.password is not None:
-        print("User.password should be None if set to None")
+        print("User.password should be None if setter to None")
 
     user_2.password = 89
     if user_2.password is not None:
-        print("User.password should be None if set to an integer")
+        print("User.password should be None if setter to an integer")
 
     if not user_1.is_valid_password(u_pwd):
-        print("is_valid_password should return True if it's the right password")
+        print("is_valid_password should return True if it's the right \
+password")
 
     if user_1.is_valid_password("Fakepwd"):
-        print("is_valid_password should return False if it's not the right password")
+        print("is_valid_password should return False if it's not the right \
+password")
 
     if user_1.is_valid_password(None):
-        print("is_valid_password should return False if compared with None")
+        print("is_valid_password should return False if compare with None")
 
     if user_1.is_valid_password(89):
-        print("is_valid_password should return False if compared with an integer")
+        print("is_valid_password should return False if compare with integer")
 
     if user_2.is_valid_password("No pwd"):
-        print("is_valid_password should return False if
+        print("is_valid_password should return False if no password set \
+before")
